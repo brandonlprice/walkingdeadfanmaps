13717736f5f4619ecebbeafa32e50ed3f6a34607
brandonlprice <37154239+brandonlprice@users.noreply.github.com>
2021-02-22 22:40:18 -0800

    Add files via upload

---
+++ survivorgroups/app/custom-scripts.js
@@ -0,0 +1,16 @@
+define(["dojo/topic","dijit/Dialog"], function(topic, Dialog) {
+  /*
+   * Custom Javascript to be executed while the application is initializing goes here
+   */
+ 
+
+
+  // The application is ready
+  topic.subscribe("tpl-ready", function(){
+    /*
+     * Custom Javascript to be executed when the application is ready goes here
+     */
+    
+
+  });
+});
